a8a7571c3a727535b3c8b245b07daf196436f65f
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-06 12:10:21 +0500

    individual bookmark component

---
+++ eru-bookmarks/src/components/Bookmark.js
@@ -0,0 +1,23 @@
+import React from "react"
+import "./Bookmark.css"
+
+const Bookmark = ({ bookmark }) => {
+  return (
+    <div className="container-bookmark">
+      <form>
+        <div className="labels">
+          <label htmlFor="name">Name:</label>
+          <label htmlFor="url">Url:</label>
+          <label htmlFor="desc">Description:</label>
+        </div>
+        <div className="inputs">
+          <input disabled name="name" value={bookmark.name} type="text" />
+          <a href="/"><input disabled className="url" name="url" value={bookmark.url} type="text" /></a>
+          <textarea disabled name="desc" value={bookmark.desc} type="text" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default Bookmark
