ee87bd532acaf0bf36d48472387d2088f9610e04
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-06 12:09:42 +0500

    init

---
+++ eru-bookmarks/gatsby-config.js
@@ -0,0 +1,11 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
+ */
+
+module.exports = {
+  /* Your site config here */
+  plugins: [],
+}
+export { wrapRootElement } from "./src/apollo/wrap-root-element"
