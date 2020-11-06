be79e1c36159ae70d0a548059b6ee9ccab80adb6
Shayaan Ahmed Farooqi <fleshlime.1@gmail.com>
2020-11-06 12:10:30 +0500

    index with bookmark list as RAM grid

--- eru-bookmarks/src/pages/index.js
+++ eru-bookmarks/src/pages/index.js
@@ -1,5 +1,50 @@
 import React from "react"
+import AddBookmark from "../components/AddBookmark"
+import Bookmark from "../components/Bookmark"
+import "./index.css"
+
+const bookmarks = [
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+  {
+    name: "GOOGLE",
+    url: "www.google.com",
+    desc: "THE SEARCH ENGINE",
+  },
+]
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <AddBookmark />
+      <div className="bookmark-list">
+        {bookmarks.map((b, i) => {
+          return <Bookmark bookmark={b} key={i} />
+        })}
+      </div>
+    </>
+  )
 }
